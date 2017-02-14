470687299c0ce497bd0e6bee4dba502088e3fb85
Predrag Cokulov <predragcokulov@gmail.com>
2017-02-14 14:36:10 +0100

    added AndroidBootstrap, initialize all android components with one line

---
+++ android/src/main/java/com/tehnicomsolutions/http/AndroidBootstrap.java
@@ -0,0 +1,32 @@
+package com.tehnicomsolutions.http;
+
+import android.content.Context;
+
+/**
+ * Created by pedja on 23.5.16. 15.45.
+ * This class is part of the ts-http
+ * Copyright © 2016 ${OWNER}
+ */
+public class AndroidBootstrap
+{
+    private static boolean initialized;
+
+    public static synchronized void initialize(Context context)
+    {
+        if(initialized)
+        {
+            throw new IllegalStateException("You can only initialize AndroidBootstrap once");
+        }
+        Network network = new AndroidNetwork(context.getApplicationContext());
+        Internet internet = new AndroidInternet(context.getApplicationContext());
+        UI ui = new AndroidUI(context.getApplicationContext());
+        TextManager textManager = new AndroidTextManager(context.getApplicationContext());
+
+        Http.initialize(network, internet, ui, textManager);
+
+        RequestManager.initialize(new AndroidRequestManager());
+
+        initialized = true;
+    }
+
+}
