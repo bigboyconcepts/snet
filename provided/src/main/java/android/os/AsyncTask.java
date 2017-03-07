0924840f4a726b282ebc1b444a113a14839c3f1b
Predrag Cokulov <predragcokulov@gmail.com>
2017-03-07 16:23:46 +0100

    [provided]updated AsyncTask signature

--- provided/src/main/java/android/os/AsyncTask.java
+++ provided/src/main/java/android/os/AsyncTask.java
@@ -10,7 +10,10 @@ public abstract class AsyncTask<Params, Progress, Result>
 {
     public static final Executor THREAD_POOL_EXECUTOR = null;
     protected abstract Result doInBackground(Params... params);
-    public void cancel(boolean mayInterruptIfRunning){}
+    public final boolean cancel(boolean mayInterruptIfRunning)
+    {
+        throw new RuntimeException("Stub!");
+    }
     protected void onPreExecute(){}
     protected void onPostExecute(Result result){}
     protected void onCancelled(Result result){}
